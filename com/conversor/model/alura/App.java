642315a8417ee78aa6bf9634c888906642cb7bf3
Charles <97968665+garfield-gar@users.noreply.github.com>
2023-08-02 20:11:04 -0300

    Add files via upload

--- com/conversor/model/alura/App.java
+++ com/conversor/model/alura/App.java
@@ -1,95 +1,55 @@
 package br.com.conversor.model.alura;
+
 import javax.swing.JOptionPane;
 
-import br.com.conversor.model.alura.Moeda;
+public class App {
 
-public  class  App  {
+    public static void main(String[] args) {
 
-	public static void main(String[] args)  {
-		
-		Moeda moeda = new Moeda();
-		String resp;
+        Moeda moeda = new Moeda();
+        String resp;
 
+        // Opções para o dropdown
+        String[] opcoes = { "Conversor de Moeda", "Conversor de Temperatura" };
 
-		// Opções para o dropdown
-		String[] opcoes = { "Conversor de Moeda", "Conversor de Temperatura" };
+        // Mostra o dropdown usando o JOptionPane
+        String selecao = (String) JOptionPane.showInputDialog(null, "Escolha uma opção:", "Menu",
+                JOptionPane.PLAIN_MESSAGE, null, opcoes, opcoes[0] // Opção selecionada por padrão
+        );
 
-		// Mostra o dropdown usando o JOptionPane
-		String selecao = (String) JOptionPane.showInputDialog(null, "Escolha uma opção:", "Menu",
-				JOptionPane.PLAIN_MESSAGE, null, opcoes, opcoes[0] // Opção selecionada por padrão
-				);
+        if (selecao != null) {
+            int resposta = JOptionPane.showOptionDialog(null,
+                    " ", "Conversor de Moedas", JOptionPane.DEFAULT_OPTION,
+                    JOptionPane.INFORMATION_MESSAGE, null, new String[] {
+                            "Dolares", "Euro", "LIBRAS ESTARLINAS", "peso Argentino", "peso Chileno"
+                    }, "default");
 
-		if(selecao != null) {
-			int resposta = JOptionPane.showOptionDialog(null,
-					" ", "Conversor de Moedas",JOptionPane.DEFAULT_OPTION,
-					JOptionPane.INFORMATION_MESSAGE,null, new String[] {
-							"Dolares", "Euro", "LIBRAS ESTARLINAS", "peso Argentino", "peso Chileno"
-			},"default");
+            // tratamento da resposta do botão clicado
+            resp = JOptionPane.showInputDialog(null, "Digite um valor:");
+            resp = resp.trim();
+            moeda.setValor(Double.parseDouble(resp));
 
-			// tratamento da resposta do botão clicado
-			switch(resposta) {
+            // Perform the conversion based on the selected currency
+            String[] currencies = { "dolares", "euro", "libras esterlinas", "peso argentino", "peso chileno" };
+            String tipoConversao = "de reais a " + currencies[resposta];
+            double convertedValue = moeda.converter(tipoConversao);
+            JOptionPane.showMessageDialog(null,
+                    "O valor convertido é: " + String.format("%.2f",convertedValue),
+                    "Message",
+                    JOptionPane.INFORMATION_MESSAGE);
 
-			case 0:
-				resp =JOptionPane.showInputDialog(null,"Digite um valor:");
-				resp.trim();
-				moeda.conver(resp);
+        } else {
+            int resposta = JOptionPane.showOptionDialog(null,
+                    " ", "Conversor de Moedas", JOptionPane.DEFAULT_OPTION,
+                    JOptionPane.INFORMATION_MESSAGE, null, new String[] {
+                            "Celsius", "Fahrenheit", "Kelvin"
+                    }, "default");
 
+            // tratamento da resposta do botão clicado
+            resp = JOptionPane.showInputDialog(null, "Digite um valor em Celsius:");
+            resp = resp.trim();
 
-				break;
-			case 1:
-				resp = JOptionPane.showInputDialog(null,"Digite um valor:");
-				resp.trim();
-				moeda.conver(resp);
-				break;
-			case 2:
-				resp = JOptionPane.showInputDialog(null,"Digite um valor:");
-				resp.trim();
-				moeda.conver(resp);
-				break;
-			case 3:
-				resp = JOptionPane.showInputDialog(null,"Digite um valor:");
-				resp.trim();
-				moeda.conver(resp);
-				break;
-			case 4:
-				resp = JOptionPane.showInputDialog(null,"Digite um valor:");
-				resp.trim();
-				moeda.conver(resp);
-				break;
-
-
-			}
-		}else {
-			int resposta = JOptionPane.showOptionDialog(null,
-					" ", "Conversor de Moedas",JOptionPane.DEFAULT_OPTION,
-					JOptionPane.INFORMATION_MESSAGE,null, new String[] {
-							"Celsius", "Fahrenheit", "Kelvin"
-			},"default");
-
-			// tratamento da resposta do botão clicado
-			switch(resposta) {
-
-			case 0:
-				resp = JOptionPane.showInputDialog(null,"Digite um valor em Celsius:");
-				resp.trim();
-
-				break;
-			case 1:
-				resp = JOptionPane.showInputDialog(null,"Digite um valor em Celsius:");
-				resp.trim();
-				break;
-			case 2:
-				resp = JOptionPane.showInputDialog(null,"Digite um valor em Celsius:");
-				resp.trim();
-				break;
-
-			}
-		}
-
-		JOptionPane.showMessageDialog(null,
-				moeda.toString(),
-				"Message",
-				JOptionPane.INFORMATION_MESSAGE);
-	}
-	
+            // Perform the temperature conversion (implement this part as needed)
+        }
+    }
 }
--- com/conversor/model/alura/Moeda.java
+++ com/conversor/model/alura/Moeda.java
@@ -1,87 +1,104 @@
 package br.com.conversor.model.alura;
 
-import br.com.conversor.model.alura.teste.App;
-
 public class Moeda {
 
-    private double valor;  
-    
-    
+	private double valor;
 
-    // constructors
-    public Moeda() {
+	// constructors
+	public Moeda() {
 
-    }
-    
-    
-    public double converter(String tipoConversao) {
-      
-        tipoConversao = tipoConversao.trim().toLowerCase();
+	}
 
-       // conversão de reais para outras moedas
-        if (tipoConversao.equals("de reais a dolares")) {
-            return this.valor / 4.37;
-        }
+	public double converter(String tipoConversao) {
 
-        if (tipoConversao.equals("de reais a euros")) {
-            return this.valor / 0.90;
-        }
+		tipoConversao = tipoConversao.trim().toLowerCase();
 
-        if (tipoConversao.equals("de reais para libras esterlinas")) {
-            return this.valor / 0.16;
-        }
+		// conversão de reais para outras moedas
+		if (tipoConversao.equals("de reais a dolares")) {
+			return this.valor / 4.72;
+		}
 
-        if (tipoConversao.equals("de reais para peso argentino")) {
-            return this.valor / 57.87;
-        }
+		if (tipoConversao.equals("de reais a euros")) {
+			return this.valor / 0.90;
+		}
 
-        if (tipoConversao.equals("de reais para peso chileno")) {
-            return this.valor / 175.02;
-        }
+		if (tipoConversao.equals("de reais para libras esterlinas")) {
+			return this.valor / 0.16;
+		}
 
-        // Conversão de outras moedas para reais 
-        if (tipoConversao.equals("de dolares a reais")) {
-            return this.valor * 4.37;
-        }
+		if (tipoConversao.equals("de reais para peso argentino")) {
+			return this.valor / 57.87;
+		}
 
-        if (tipoConversao.equals("de euros a reais")) {
-            return this.valor * 0.90;
-        }
+		if (tipoConversao.equals("de reais para peso chileno")) {
+			return this.valor / 175.02;
+		}
 
-        if (tipoConversao.equals("de libras esterlinas a reais")) {
-            return this.valor * 0.16;
-        }
+		// Conversão de outras moedas para reais
+		if (tipoConversao.equals("de dolares a reais")) {
+			return this.valor * 4.37;
+		}
 
-        if (tipoConversao.equals("de peso argentino a reais")) {
-            return this.valor * 57.87;
-        }
+		if (tipoConversao.equals("de euros a reais")) {
+			return this.valor * 0.90;
+		}
 
-        if (tipoConversao.equals("de peso chileno a reais")) {
-            return this.valor * 175.02;
-        }
+		if (tipoConversao.equals("de libras esterlinas a reais")) {
+			return this.valor * 0.16;
+		}
 
-        // If the conversion is not recognized, return an error value (e.g., -1)
-        return this.valor;
-    }
+		if (tipoConversao.equals("de peso argentino a reais")) {
+			return this.valor * 57.87;
+		}
 
-    
-      
-    public double getValor() {
-    	String resp = String.valueOf(valor);
-        return valor;
-    }
+		if (tipoConversao.equals("de peso chileno a reais")) {
+			return this.valor * 175.02;
+		}
 
-    public String toString() {
-        String tipoConversao = null;
-		return "O valor da conversão é de " + this.converter(tipoConversao);
-    }
+		// If the conversion is not recognized, return an error value (e.g., -1)
+		return -1;
+	}
+	
+	public double converterTemperatura(String tipoConversao) {
+
+		tipoConversao = tipoConversao.trim().toLowerCase();
+
+		// conversão de reais para outras moedas
+		if (tipoConversao.equals("Celsius em Fahrenheit")) {
+			return this.valor * 1.8 + 32;
+		}
+
+		if (tipoConversao.equals("Celsius em kelvin")) {
+			return this.valor + 273.15;
+		}
+
+		
+
+		
+		// Conversão reversa da temperatura
+		if (tipoConversao.equals("Fahrenheit em Celsius")) {
+			return (this.valor - 32)/ 1.8;
+		}
+
+		if (tipoConversao.equals("Kelvin em celsius")) {
+			return this.valor - 273.15;
+		}
+
+		
+
+		// If the conversion is not recognized, return an error value (e.g., -1)
+		return -1;
+	}
+	
+	public double getValor() {
+		return valor;
+	}
+
+	
 
 	public void setValor(double valor) {
-		this.valor=valor;
-		
-		}
+		this.valor = valor;
 
-	public void conver(String resp) {
-		double value= Double.parseDouble(resp);
-		}
+	}
+
 }
